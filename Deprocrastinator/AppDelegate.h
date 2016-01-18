6f7064c53b08998e959c53652dfedf6d46063ed9
Brett Tau <brettTau@bretts-macbook-pro.local>
2016-01-18 13:54:55 -0600

    Initial Commit

---
+++ Deprocrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deprocrastinator
+//
+//  Created by Brett Tau on 1/18/16.
+//  Copyright © 2016 Brett Tau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Brett Tau on 1/18/16.
+//  Copyright © 2016 Brett Tau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
